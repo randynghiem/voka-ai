6209c6ac5977f67a01c22781009d304a4342bf9a
Randy Nghiem <randy.nghiem@hotmail.com>
2020-02-05 22:17:21 +0800

    update project structure

--- ai_main/asgi.py
+++ ai_main/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for voka_ai project.
+ASGI config for ai_main project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voka_ai.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_main.settings')
 
 application = get_asgi_application()
--- ai_main/wsgi.py
+++ ai_main/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for voka_ai project.
+WSGI config for ai_main project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voka_ai.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_main.settings')
 
 application = get_wsgi_application()
